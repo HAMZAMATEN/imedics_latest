109bbbf8c333228c13603d30a571b00d39be6cc9
Muhammad Zain <zainmughal4379329@gmail.com>
2024-04-29 13:01:02 +0500

    zain

--- lib/helpers/app_constants.dart
+++ lib/helpers/app_constants.dart
@@ -2,4 +2,8 @@ class AppConstants{
   static int index = 0;
   static int accountType = 0;
   static String userId = '';
+
+
+  static String baseUrl = 'https://imdfx-newserver-production.up.railway.app/api/';
+  static String imageBaseUrl = 'https://imdfx-newserver-production.up.railway.app/';
 }
